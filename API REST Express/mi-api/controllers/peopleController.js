3078208c8771fa926edf57796d9d7894c68a5f4e
Julia15Lop <julialopezs15@hotmail.com>
2021-05-26 22:49:12 +0200

    Recreate project

--- API REST Express/mi-api/controllers/peopleController.js
+++ API REST Express/mi-api/controllers/peopleController.js
@@ -1,3 +1,12 @@
+/* DB connection */
+var db = require('../db/db');
+
+db.connect('mongodb://localhost:27017', function (err) {
+    if (err) {
+        throw ('Fallo en la conexión con la BD');
+    }
+});
+
 // Display all people
 module.exports.people_list = function(req, res) {
     res.send('NOT IMPLEMENTED: List people');
@@ -5,5 +14,15 @@ module.exports.people_list = function(req, res) {
 
 // Create person
 module.exports.people_create = function(req, res) {
-    res.send('NOT IMPLEMENTED: Create person');
+    const person = {};
+    person.name = req.body.name;
+    person.phone = req.body.phone;
+
+    db.get().db('mydb').collection('people').insertOne(person, function(err, result) {
+        if (err) {
+            throw("Fallo en la conexión con la BD");
+        } else {
+            res.send(result);
+        }
+    });
 };
